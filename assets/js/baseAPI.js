772f363385315ba304b6fbe2aa7387668a3cb399
woaichidadatangyuan <1648542844@qq.com>
2020-12-13 21:34:59 +0800

    完成了登录和注册功能的开发

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,7 @@
+//注意：每次调用$.get(),$.post(),$.ajax()的时候
+//都会先调用ajaxPrefilter函数 在这个函数中 可以拿到ajax提供的配置对象
+$.ajaxPrefilter(function(options) {
+    //在发起真正的Ajax请求之前， 统一拼接请求的根路径
+    options.url = 'http://ajax.frontend.itheima.net' + options.url
+    console.log(options.url);
+})
